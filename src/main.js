2e08771fa12f21f96eebe555568cfce509fbc2e9
chukmunnlee <chukmunnlee@gmail.com>
2022-08-01 16:46:59 +0800

    update main.js to use async/await instead of Promise

--- src/main.js
+++ src/main.js
@@ -10,65 +10,61 @@ const app = express()
 
 app.use(morgan("dev"))
 
-app.get('/games', (req, resp) => {
+app.get('/games', async (req, resp) => {
 	const offset = parseInt(req.query.offset) || 0
 	const limit = parseInt(req.query.limit) || 10
 
-	findAllGames(offset, limit)
-		.then(result => {
-			resp.status(200)
-			resp.json(mkGameUrl(result))
-		})
-		.catch(err => {
-			resp.status(500)
-			resp.json(mkError(err))
-		})
+	try {
+		const result = await findAllGames(offset, limit)
+		resp.status(200)
+		resp.json(mkGameUrl(result))
+	} catch (err) {
+		resp.status(500)
+		resp.json(mkError(err))
+	}
 })
 
 // Games
-app.get('/games/search', (req, resp) => {
+app.get('/games/search', async (req, resp) => {
 	const q = req.query.q
 	if (!q) 
 		return resp.status(400).json({ error: `Missing query parameter q` })
 	
-	findGamesByName(q)
-		.then(result => {
-			resp.status(200)
-			resp.json(mkGameUrl(result))
-		})
-		.catch(err => {
-			resp.status(500)
-			resp.json(mkError(err))
-		})
+	try {
+		const result = await findGamesByName(q)
+		resp.status(200)
+		resp.json(mkGameUrl(result))
+	} catch (err) {
+		resp.status(500)
+		resp.json(mkError(err))
+	}
 })
 
-app.get('/games/count', (req, resp) => {
-	countGames()
-		.then(result => {
-			resp.status(200)
-			resp.json({ count: result })
-		})
-		.catch(err => {
-			resp.status(500)
-			resp.json(mkError(err))
-		})
+app.get('/games/count', async (req, resp) => {
+	try {
+		const result = await countGames()
+		resp.status(200)
+		resp.json({ count: result })
+	} catch (err) {
+		resp.status(500)
+		resp.json(mkError(err))
+	}
 })
 
-app.get('/game/:id', (req, resp) => {
-	findGameById(parseInt(req.params.id))
-		.then(result => {
-			if (!result) {
-				resp.status(404)
-				resp.json(mkError(`Cannot find gameId ${req.params.id}`))
-				return
-			}
-			resp.status(200)
-			resp.json(result)
-		})
-		.catch(err => {
-			resp.status(500)
-			resp.json(mkError(err))
-		})
+app.get('/game/:id', async (req, resp) => {
+	try {
+		const result = await findGameById(parseInt(req.params.id))
+		if (!result) {
+			resp.status(404)
+			resp.json(mkError(`Cannot find gameId ${req.params.id}`))
+			return
+		}
+		resp.status(200)
+		resp.json(result)
+	} catch(err) {
+		resp.status(500)
+		resp.json(mkError(err))
+	}
 })
 
 // Comments
